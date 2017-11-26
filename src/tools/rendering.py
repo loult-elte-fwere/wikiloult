de760c4ee99aa2657137a47b9ae766acd73fab82
Hadrien Titeux <hadwarez@gmail.com>
2017-11-26 21:15:46 +0100

    Ajout d'une classe pour faire le rendu du texte en markdown vers le HTML, avec le support des liens wiki.

---
+++ src/tools/rendering.py
@@ -0,0 +1,41 @@
+import re
+from mistune import Renderer, InlineLexer, Markdown
+
+
+class WikiLinkRenderer(Renderer):
+    def wiki_link(self, alt, link):
+        return '<a href="page/%s">%s</a>' % (link, alt)
+
+
+class WikiLinkInlineLexer(InlineLexer):
+    def enable_wiki_link(self):
+        # add wiki_link rules
+        self.rules.wiki_link = re.compile(
+            r'\[\['                   # [[
+            r'([\s\S]+?\|[a-zA-Z0-9_]+?)'   # Page du wiki|page_name
+            r'\]\](?!\])'             # ]]
+        )
+
+        # Add wiki_link parser to default rules
+        # you can insert it some place you like
+        # but place matters, maybe 3 is not good
+        self.default_rules.insert(3, 'wiki_link')
+
+    def output_wiki_link(self, m):
+        text = m.group(1)
+        alt, link = text.split('|')
+        # you can create an custom render
+        # you can also return the html if you like
+        return self.renderer.wiki_link(alt, link)
+
+
+class WikiPageRenderer:
+
+    def __init__(self):
+        wiki_link_renderer = WikiLinkRenderer()
+        link_lexer = WikiLinkInlineLexer(wiki_link_renderer)
+        link_lexer.enable_wiki_link()
+        self.renderer = Markdown(wiki_link_renderer, inline=link_lexer)
+
+    def render(self, page_string : str):
+        return self.renderer(page_string)
