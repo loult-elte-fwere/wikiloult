fa75266a03958a11fef71561ad1d39f1b24231ec
Hadrien Titeux <hadwarez@gmail.com>
2017-11-27 01:53:13 +0100

    Création d'ébauche pour les connecteurs à la base de donnée

--- src/tools/models.py
+++ src/tools/models.py
@@ -17,9 +17,33 @@ class UsersConnector(BaseConnector):
         super().__init__()
         self.users = self.db[USERS_COLLECTION_NAME]
 
+    def register_user(self, user_cookie: str):
+        """Registers user cookie as a new user. Sets authorization to pending"""
+        pass
+
+    def authorize_user(self, user_cookie: str):
+        """Authorize user to create/edit pages"""
+        pass
+
+    def get_user_data(self, user_cookie: str):
+        pass
+
+    def is_allowed(self, user_cookie: str) -> bool:
+        """Looks up if a user is allowed to edit/create pages or not"""
+        pass
+
 
 class WikiPagesConnector(BaseConnector):
 
     def __init__(self):
         super().__init__()
         self.pages = self.db[PAGES_COLLECTION_NAME]
+
+    def create_page(self, page_name : str, markdown_content: str, page_title: str, editor_cookie: str):
+        pass
+
+    def edit_page(self, page_name: str, markdown_content: str):
+        pass
+
+    def search_page(self, search_query: str):
+        pass
